b883fb59db9cce21bcfe52b7723dbab4f62ae631
MrKitty7 <107936754+MrKitty7@users.noreply.github.com>
2022-09-05 12:59:55 +0200

    Added help command, planned the project.

---
+++ help.py
@@ -0,0 +1,24 @@
+def separate():
+    print('------')
+
+def help_command():
+    separate()
+    print('List of commands:')
+    separate()
+    print('create')
+    print('add')
+    print('set')
+    print('edit')
+    print('change')
+    print('remove')
+    separate()
+    print('List of functions:')
+    print('Create a class - Command: create class')
+    print('Create a subject - Command: create subject')
+    separate()
+    print('Set subject for selected class in a selected day - Command: set subject day class-number subject')
+    separate()
+    print('Add a lesson to selected subject - Command: add lesson subject')
+    print('Add an exam to selected subject, selected date - Command: add exam subject date')
+    separate()
+
---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+from help import help_command
+
+# Arguments
+
+# Help Argument
+if sys.argv[1] == 'help':
+    help_command()
+
+
+
+#------------#
